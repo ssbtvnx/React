2dd9fe6988f7ffa67cf58fd349a8a36438f45d0d
ssbtvnx <sabinaashimbekova@gmail.com>
2025-09-25 15:42:40 +0500

    hw2

---
+++ hw2/react-app/src/AboutMe.jsx
@@ -0,0 +1,22 @@
+import "./AboutMe.css";
+
+function AboutMe() {
+  return (
+    <section className = "about-section">
+      <div className = "about-text">
+            <h1>About me</h1>
+             <span className="underline"></span>
+            <h2>Sabina Ashimbekova-student</h2>
+            <p>
+              University: Kazakh-British Technical University<br />
+              Faculty, Course: SITE, 4rd<br />
+              Speciality: Information Systems<br />
+            </p>
+            <h2>Contacts:</h2>
+            <p>e-mail: s_ashimbekova@kbtu.kz</p>
+      </div>
+    </section>
+  )
+}
+
+export default AboutMe;
---
+++ hw2/react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import AboutMe from "./AboutMe"
+
+function App() {
+  return (
+    <div className="App">
+      <AboutMe />
+    </div>
+  )
+}
+
+export default App
